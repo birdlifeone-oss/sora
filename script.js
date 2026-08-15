9b876d33ac5dc416c09a708b4dce0617f77d6448
birdlifeone-oss <birdlifeone@gmail.com>
2026-08-15 17:37:52 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-const fadeOnlytargets = document.querySelectorAll('.fadeOnlyTrigger');
-const fadeSlidetargets = document.querySelectorAll('.fadeSlideTrigger');
+const fadetexttargets = document.querySelectorAll('.fadetextTrigger');
 
-const options = {
-  root: null,
-  rootMargin: '0px 0px 0px 0px',
-  threshold: 0
-};
 
 const observerFade = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       // 要素が見えたら実行する処理
-      entry.target.classList.add('fadeOnly');
+      entry.target.classList.add('fadetext');
     }
   });
 },{
@@ -21,18 +15,32 @@ const observerFade = new IntersectionObserver((entries) => {
 });
 
 
+fadetexttargets.forEach(target => observerFade.observe(target));
+
+
+const fadeimgtargets = document.querySelectorAll('.fadeimgTrigger');
+
+// 画面幅で条件分岐
+const isPC = window.matchMedia('(min-width: 1025px)').matches;
+
+// PC：画像が半分見えたら → threshold: 0.5  
+// タブレット以下：画像が全部入ったら → threshold: 1
+const imgThreshold = isPC ? 0.5 : 1;
+
+// rootMargin も必要ならここで分岐可能
+const imgRootMargin = isPC ? '0px 0px -5% 0px' : '0px 0px 0px 0px';
+
 const observerSlide = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      // 要素が見えたら実行する処理
-      entry.target.classList.add('fadeSlide');
+      entry.target.classList.add('fadeimg');
     }
   });
 },{
   root: null,
-  rootMargin: '0px 0px -5% 0px',
-  threshold: 1
+  rootMargin: imgRootMargin,
+  threshold: imgThreshold
 });
 
-fadeOnlytargets.forEach(target => observerFade.observe(target));
-fadeSlidetargets.forEach(target => observerSlide.observe(target));
+fadeimgtargets.forEach(target => observerSlide.observe(target));
+
